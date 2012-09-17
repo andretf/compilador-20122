f9caf6479130982c0ea68f0619d2c350412470e4
andretf@gmail.com <andretf@gmail.com@e3602872-7008-8575-098f-46a3b9d8b4de>
2012-09-17 23:32:36 +0000

    + add

---
+++ trunk/etapa1novo/main.c
@@ -0,0 +1,14 @@
+int main(int argc, char *argv[]) {
+	int token;
+	int running = 1;
+
+	initMe();
+	while(running) {
+		token = yylex();
+		if (token == 0) 
+			running = 0;			
+		classify(token);
+	}
+	finishMe();
+
+}
